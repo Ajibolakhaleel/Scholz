f919b22a47e4941bf978327f88cd62c2156f5ea2
Ajibolakhaleel <k.ajibola@alustudent.com>
2024-07-31 18:13:23 +0200

    5

---
+++ Scholz backend/index.js
@@ -0,0 +1,42 @@
+// index.js
+import express from 'express';
+import 'dotenv/config';
+import bodyParser from 'body-parser';
+import cors from 'cors'; 
+import router from './src/routes/authRoutes.js';
+import { subjectRoutes } from './src/routes/subjectRouter.js';
+import { questionRoutes } from './src/routes/questionRouter.js';
+import { pointRoutes } from './src/routes/pointRouter.js';
+import { userRoutes } from './src/routes/userRouter.js';
+import swaggerFile from './src/swagger/swagger-output.json' assert { type: 'json' };
+import swaggerUi from 'swagger-ui-express';
+
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use('/auth', router);
+app.use(subjectRoutes);
+app.use(questionRoutes);
+app.use(pointRoutes);
+app.use(userRoutes);
+
+// Swagger UI setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Start the server
+const PORT = process.env.PORT || 3100;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
